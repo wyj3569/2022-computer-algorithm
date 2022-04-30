a587b3fecfe33519c5d0e5c6c8a416105b32deee
WooYoung <wyjung0731@gmail.com>
2022-04-30 18:36:30 +0900

    delete files

---
+++ 2번_뒤로가기/back.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+string input, target;
+
+void input_data() {
+    cin >> input;
+    cin >> target;
+}
+
+int main() {
+    cin >> N;
+    while (N--) {
+        input_data();
+    }
+    return 0;
+}
